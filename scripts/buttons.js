c3178d1debe495f23e6bcfc5916cff3640bf3dba
KAGmay05 <kelenalfarogarcia@gmail.com>
2025-09-17 09:30:08 +0000

    Added changes to the main page

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -29,3 +29,37 @@ function Contacto(){
 function Dist(){
      window.location.href = "index5.html";
 }
+
+//carrusel
+let indice = 0;
+const slides = document.querySelectorAll('.slide');
+const slidesPerView = 3;
+const groupIndices = [0, 2];
+const totalGroups = groupIndices.length;
+
+function moverSlide(direccion) {
+    indice += direccion;
+
+    if (indice < 0) indice = totalGroups - 1;
+    if (indice >= totalGroups) indice = 0;
+
+    const startIndex = groupIndices[indice];
+    const desplazamiento = -(startIndex / slidesPerView) * 100;
+
+    document.querySelector(".slides").style.transform = `translateX(${desplazamiento}%)`;
+}
+
+//cortina
+const cortina = document.getElementById("cortina");
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
+
+//abrir cortina
+arrowLeft.addEventListener('click', () => {
+     cortina.classList.add("cortina-activa");
+});
+
+//cerrar cortina
+arrowRight.addEventListener('click', () => {
+     cortina.classList.remove("cortina-activa");
+});
